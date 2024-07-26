70d22c40b5e94484b211c2b20fca51633b2bd99c
johntec-ads <johntec.ads@gmail.com>
2024-07-26 06:59:20 -0300

    upFirebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { useState } from 'react';
 import { db } from './firebaseConection'
+import { doc, setDoc } from 'firebase/firestore';
+
 import './app.css';
 
 function App () {
 
-  const {titulo, setTitulo} = useState('');
-  const {autor, setAutor} = useState('');
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
 
-  function handleAdd() {
-    alert( 'TESTE ')
+  async function handleAdd() {
+    await setDoc( doc(db, "post", "12345"), {
+      titulo: titulo,
+      autor: autor,
+    } )
+    .then(() => {/* promise sucess */
+      console.log("Dados Registrados no BD")
+
+    })
+    .catch((error) => {/* promise error */
+      console.log("Gerou Erro" + error)
+    })
   }
 
 
@@ -24,13 +36,15 @@ function App () {
           type='text'
           placeholder='Digite o titulo'
           value={titulo}
-          onChange={ (e) => setTitulo(e.target.valeu) }
+          onChange={ (e) => setTitulo(e.target.value) }
         />
 
         <label>Autor</label>
         <input
           type='text'
           placeholder='Autor do post'
+          value={autor}
+          onChange={(e) => setAutor(e.target.value)}
         />
 
         <button onClick={handleAdd} >Cadastrar</button>
