9cb645e45a9c87279fe3a64e0f2e643566b10071
John Marcelo <johntec.ads@gmail.com>
2024-07-25 22:12:25 -0300

    firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-
+import { useState } from 'react';
 import { db } from './firebaseConection'
+import './app.css';
 
-function App() {
-  return(
+function App () {
+
+  const {titulo, setTitulo} = useState('');
+  const {autor, setAutor} = useState('');
+
+  function handleAdd() {
+    alert( 'TESTE ')
+  }
+
+
+
+  return (
     <div>
-      <h2>
-        React JS + Firebase
-      </h2>
+      <h1> React JS + Firebase </h1>
+
+      <div className='container' >
+
+        <label> Titulo: </label>
+        <textarea
+          type='text'
+          placeholder='Digite o titulo'
+          value={titulo}
+          onChange={ (e) => setTitulo(e.target.valeu) }
+        />
+
+        <label>Autor</label>
+        <input
+          type='text'
+          placeholder='Autor do post'
+        />
+
+        <button onClick={handleAdd} >Cadastrar</button>
+
+      </div>
+
+
     </div>
   )
 }
