bec1c2907b0b147a6b1b7147a8720b953bb31570
John Marcelo <johntec.ads@gmail.com>
2024-07-27 18:27:53 -0300

    upFireBase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { db } from './firebaseConection'
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore';
 
 import './app.css';
 
@@ -9,20 +9,75 @@ function App () {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
 
+  const [posts, setPosts] = useState([]);
+
   async function handleAdd() {
-    await setDoc( doc(db, "post", "12345"), {
+/*     await setDoc( doc(db, "post", "12345"), {
       titulo: titulo,
       autor: autor,
     } )
-    .then(() => {/* promise sucess */
+    .then(() => {
       console.log("Dados Registrados no BD")
 
     })
-    .catch((error) => {/* promise error */
+    .catch((error) => {
       console.log("Gerou Erro" + error)
-    })
+    }) */
+
+      await addDoc(collection(db, "post"), {
+        titulo: titulo,
+        autor: autor,
+      })
+      .then(() => {
+        console.log("Cadastrado com sucesso")
+        setAutor('')
+        setTitulo('')
+      })
+      .catch((error) => {
+        console.log("Errou!" + error)
+      })
+
   }
 
+
+  async function buscarPost() {
+    /* const postRef = doc(db,"post", "HxGXvIECY38SCHuy8OGv" )
+    await getDoc(postRef)
+    .then((snapshot) => {
+      setAutor(snapshot.data().autor)
+      setTitulo(snapshot.data().titulo)
+    })
+    .catch((error) => {
+      console.log("Errou!" + error)
+    }) */
+
+      const postsRef = collection(db, "post")
+      await getDocs(postsRef)
+      .then((snapshot) => {
+        let lista = []
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          })
+        })
+
+        setPosts(lista);
+
+      })
+      .catch((error) => {
+        console.log("Errou ao buscar "+error)
+
+      })
+
+
+
+
+
+
+  }
 
 
   return (
@@ -48,6 +103,25 @@ function App () {
         />
 
         <button onClick={handleAdd} >Cadastrar</button>
+        <button onClick={buscarPost} >Buscar posts</button>
+
+        <ul>
+  {posts
+    .sort((a, b) => {
+      const numA = parseInt(a.titulo.split(' ')[2]);
+      const numB = parseInt(b.titulo.split(' ')[2]);
+      return numA - numB;
+    })
+    .map((post) => {
+      return (
+        <li key={post.id}>
+          <span>Titulo: {post.titulo} </span>
+          <span>Autor: {post.autor} </span>
+        </li>
+      );
+    })}
+</ul>
+
 
       </div>
 
