0c73de5304f78d672c185a747114c4b9805ec2ed
John Marcelo <johntec.ads@gmail.com>
2024-07-30 20:23:22 -0300

    FIREBASE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { db } from './firebaseConnection';/* Import realizado com 
-chaves, devido à exportação não ter sido default */
+chaves, devido à exportação não ter sido default           */
 
 
 function App () {
   return (
     <div>
-      <h2>React + Firebase</h2>
+      <h2>React + Firebase ;-)</h2>
     </div>
   )
 }
