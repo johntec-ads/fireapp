c48c5b6f0df6ca9aae007d14cfe3e1f6c4b69338
johntec-ads <johntec.ads@gmail.com>
2024-07-30 06:37:21 -0300

    upCursoFirebase

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+/* 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgTFpZIlOhyzxzCr-g8u4EaPYCGW4MJXE",
+  authDomain: "cursoprojeto-a7cb1.firebaseapp.com",
+  projectId: "cursoprojeto-a7cb1",
+  storageBucket: "cursoprojeto-a7cb1.appspot.com",
+  messagingSenderId: "495630865353",
+  appId: "1:495630865353:web:34bf580ca757276edfce18",
+  measurementId: "G-JQR1V9Q74W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+ */
