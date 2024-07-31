91b1aab47410bac0fa670ceed5bf4f4c829df2a3
johntec-ads <johntec.ads@gmail.com>
2024-07-31 06:56:41 -0300

    firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {
   setDoc,/* setDoc: Cria ou sobrescreve um documento no firestore */
   collection,//O método collection é usado para acessar uma coleção no Firestore.
   addDoc,//O método addDoc é usado para adicionar um novo documento a uma coleção.
-  getDoc,//getDoc é usado para recuperar dados de um documento específico no Firestore 
+  getDoc,//getDoc é usado para recuperar dados de um documento específico no Firestore .
+  getDocs,//Método usado para executar consultas em coleções ou grupos de coleções.
 
 } from 'firebase/firestore';
 
@@ -17,6 +18,9 @@ function App () {
   const [ titulo, setTitulo ] = useState( '' );
   const [ autor, setAutor ] = useState( '' );
 
+  const [posts, setPosts] = useState([]);//state para obter as coleções de doc
+
+  /* CADASTRAR UM ITEM ÚNICO */
   async function handleAdd () {
     /*await aguarda - addDoc cria um cocumento-collection :acessa uma coleção */
     await addDoc( collection( db, "posts" ), {
@@ -37,7 +41,9 @@ function App () {
       } )
   }
 
-  async function buscarPost() {
+  async function buscarPost() {    
+   /*  //BUSCANDO UM POST ESPECÍFICO
+
     //obtendo na const postRef na colection posts, o documento 12345
     const postRef = doc(db, "posts", "12345")
     //retorno com getDoc dos itens
@@ -45,11 +51,35 @@ function App () {
     .then( (snapshot) => {//snapshot=informações rápidas de quaisquer evento que a app sofreu
       setAutor(snapshot.data().autor)//retorna os dados do db
       setTitulo(snapshot.data().titulo)
-
     })
     .catch( () => {
       console.log('Erro ao buscar')
-    })
+    }) */
+
+      //BUSCANDO VARIOS POSTS DE UMA COLEÇÃO
+     
+      const postsRef = collection(db, "posts") //Declara um referência e obtém a colection
+      await getDocs(postsRef)
+      .then((snapshot) => {
+        console.log('Sucesso na busca')
+
+        let lista = snapshot.docs.map((doc) => {
+        return{
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        }
+      })
+      setPosts(lista)
+      console.log(lista)
+      })
+      .catch((error) => {
+        console.log('Erro na busca' +error)
+      })
+
+
+
+
   }
 
 
@@ -82,6 +112,21 @@ function App () {
         <button onClick={ handleAdd } >Cadastrar</button>
         <button onClick={buscarPost} >Buscar post</button>
 
+        <ul>
+          {posts.map((item) => {
+            return (
+               <li key={item.id}>
+              <strong>ID: {item.id} </strong> <br/>
+              <span>Titulo: {item.titulo} </span> <br/>
+              <span>Autor:  {item.autor} </span>
+
+            </li>
+            )
+           
+          })}
+        </ul>
+
+        
       </div>
 
 
