723419ff5da13dd1103ba19aec315d478f4d8156
John Marcelo <johntec.ads@gmail.com>
2024-08-04 10:14:19 -0300

    upProject

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,10 +1,52 @@
 
-import React from 'react';
+import { useState } from 'react';
+import './admin.css'
+
+import { auth } from '../../firebaseConnection'
+import { signOut } from 'firebase/auth';
+
 
 function Admin() {
+
+  const [tarefaInput, setTarefaInput] = useState('');
+
+   function handleRegister(e) {
+    e.preventDefault();/* Metodo para não atualizar a página */
+
+    alert('register tarefa ok')
+   }
+
+   async function handleLogout() {
+    await signOut(auth);//Para logout, passa o component auth dentro do signOut
+   }
+
   return(
-    <div>
-      <h1>Pagina Admin</h1>
+    <div className='admin-container'>
+      
+      <h1>Minhas tarefas</h1>
+
+      <form className='form' onSubmit={handleRegister} > 
+        <textarea
+          placeholder='Digite sua tarefa...'
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+
+        />
+        <button className='btn-register' type='submit' >Registrar tarefa</button>{/* 'submit' porque esta dentro da
+        tag de <form/> */}
+      </form>
+
+      <article className='list' >
+        <p>Estudar javascript e reactjs no fim de semana</p>
+
+        <div>
+          <button>Editar</button>
+          <button className='btn-delete' >Concluir</button>
+        </div>
+      </article>
+
+      <button className='btn-logout' onClick={handleLogout} >Sair</button>
+
     </div>
   )
 }
